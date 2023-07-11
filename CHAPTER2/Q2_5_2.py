124bb104a2e50bd734a104535cf52c98ea79b021
Unten Suzuka <n23005@std.it-college.ac.jp>
2023-07-11 00:54:17 +0000

    2.5やり直し

--- CHAPTER2/Q2_5_2.py
+++ CHAPTER2/Q2_5_2.py
@@ -1,2 +1,24 @@
-"abc" + "5"
-# エラーが出るやつ
+# 文字列を書いた作り方
+answer = """○ ● ● ●
+● ○ ● ●
+● ● ○ ●
+● ● ● ○
+"""
+print(answer)
+
+# 制御文字と掛け算
+w = "○ "
+b = "● "
+answer = w + b * 3 + "\n" + b + w + b * 2 + "\n" + b * 2 + w + b + "\n" + b * 3 + w
+print(answer)
+
+# for,if
+answer = ""
+for i in range(4):
+    for j in range(4):
+        if i == j:
+            answer += w
+        else:
+            answer += b
+    answer += "\n"
+print(answer)
