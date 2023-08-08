fef6161ce43d5bca8f4600d042058a0fe7550a5b
Unten Suzuka <n23005@std.it-college.ac.jp>
2023-08-08 07:57:52 +0000

    Q4_8

---
+++ CHAPTER4/Q4_8_1.py
@@ -0,0 +1,23 @@
+def show_begin_end(func):
+    """呼ばれた関数の始めと終わりを表示するデコレータ"""
+
+    def deco_func(*args, **kwargs):
+        """関数を実行する前と後にメッセージを表示"""
+        print("== start")
+        result = func(*args, **kwargs)
+        print("== end")
+        return result
+
+    return deco_func
+
+
+@show_begin_end
+def sleep_for_a_while():
+    """しばらく眠る"""
+    print("sleeping ..")
+    print("Done Sleeping")
+
+
+sleep_for_a_while()
+
+# 2秒待機する関数の作成は省略しています(汗)
