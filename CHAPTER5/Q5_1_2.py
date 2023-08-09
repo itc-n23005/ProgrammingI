eded63ad1adee9456d80d3b5d2dd413f11ab9f5a
Unten Suzuka <n23005@std.it-college.ac.jp>
2023-08-09 00:39:37 +0000

    Q5_1

---
+++ CHAPTER5/Q5_1_2.py
@@ -0,0 +1,11 @@
+stack = []
+stack.append(1)
+print("stack:", stack)
+stack.append(2)
+print("stack:", stack)
+print("pop 1st value:", stack.pop())
+print("pop 2nd value:", stack.pop())
+print("stack:", stack)
+
+# 6行目(1回目の).pop()でstackリストに後に追加した'2'を
+# 取り出したリストをプリント
