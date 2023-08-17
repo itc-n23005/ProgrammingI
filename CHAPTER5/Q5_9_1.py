8336dcc69adff921ab78d164a3248843ee4eadd2
Unten Suzuka <n23005@std.it-college.ac.jp>
2023-08-17 07:53:09 +0000

    Q5_9

---
+++ CHAPTER5/Q5_9_1.py
@@ -0,0 +1,18 @@
+mountain_in_japan = {"fuji": 3776, "kitadake": 3193, "okuhodakadake": 3190, "dummy": 0}
+mountain_in_japan_sorted = sorted(mountain_in_japan.items(), key=lambda x: x[1])
+for key, val in reversed(mountain_in_japan_sorted):
+    print(key, val)
+
+# こう出る ↓
+# fuji 3776
+# kitadake 3193
+# okuhodakadake 3190
+# dummy 0
+
+mountain_in_japan_sorted = sorted(
+    mountain_in_japan.items(), key=lambda x: x[1], reverse=True
+)
+print(mountain_in_japan_sorted)
+
+# 上の3行目を12行目のように書くとこう出る ↓
+# [('fuji', 3776), ('kitadake', 3193), ('okuhodakadake', 3190), ('dummy', 0)]
